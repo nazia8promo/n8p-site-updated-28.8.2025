5d27d1d8d30d56787ebf9fb95f8cef3aface8cf2
nazia8promo <nazia8promo@gmail.com>
2026-01-14 19:46:39 +0500

    fix: correct vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/nazia8promo/n8p-site-updated-28.8.2025/", // ⚠️ ИМЯ РЕПОЗИТОРИЯ НА GITHUB
+  base: "/n8p-site-updated-28.8.2025/",
 });
